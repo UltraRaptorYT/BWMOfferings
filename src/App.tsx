8934e1aebeafbe259fda70e2fb763eebd2766302
Soh Hong Yu <raymondng009@gmail.com>
2024-12-06 15:52:01 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>hi</>;
+  return <div>hi</div>;
 }
 
 export default App;
